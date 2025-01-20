704509b0c8516e358883db61e0bbb1a325ecedef
snwmilap <ronak@softnweb.in>
2025-01-20 14:56:45 +0530

    Update sanity.ts

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -5,7 +5,7 @@ const sanityClient = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '',
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || '',
   useCdn: process.env.NODE_ENV === 'production',
-  apiVersion: '2023-01-01', // Update this to your API version
+  apiVersion: 'v2021-03-25', // Update this to your API version
 });
 
 export default sanityClient;
